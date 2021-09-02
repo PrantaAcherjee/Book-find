c0279c83f2b8088859dd7c664055dc4d5e2e4901
PrantaAcherjee <PrantaAcharjee647@gmail.com>
2021-09-02 22:58:12 +0600

    after final correction

--- index.js
+++ index.js
@@ -1,43 +1,66 @@
-const bookCollection = () => {
-    const searchField = document.getElementById('search-field');
-    const searchText = searchField.value;
-    const errorDiv = document.getElementById('errors');
-    console.log(searchText);
-    searchField.value = '';
-    if (searchText == '') {
-        errorDiv.innerText = 'Search can not be empty !!'
+// Connect With DOM 
+const inputText = document.getElementById('input-value');
+const spinner = document.getElementById('spinner')
+const errorDiv = document.getElementById('error')
+const totalBooks = document.getElementById('total-books');
+const parentDiv = document.getElementById('search-result');
+
+// get input value and fetch data
+const searchField = () => {
+    spinner.classList.remove("d-none")
+    const inptuValue = inputText.value;
+    // Input Error Handling
+    if (inptuValue === "") {
+        parentDiv.textContent = '';
+        totalBooks.innerHTML = '';
+        errorDiv.innerText = "Search field Cannot be empty."
+        spinner.classList.add("d-none")
+        return;
     }
-    else { errorDiv.innerText = 'your data are given below !!' }
-
-
-    const url = `https://openlibrary.org/search.json?q=${searchText}`
-    console.log(url);
-
-    fetch(url)
-        .then(responsive => responsive.json())
-        .then(data => displaySearchResult(data.docs))
-
+    // Clear 
+    errorDiv.innerText = "";
+    inputText.value = "";
+    totalBooks.innerHTML = '';
+    parentDiv.textContent = '';
+    // Get Api 
+    fetch(`https://openlibrary.org/search.json?q=${inptuValue}`)
+        .then(res => res.json())
+        .then(data => displaySearchResult(data))
 }
+
+// Display Search Result
 const displaySearchResult = books => {
-    const searchResult = document.getElementById('search-result');
-    searchResult.innerHTML = '';
-    books.forEach(book => {
-        // console.log(book);
+    spinner.classList.add("d-none")
+    if (books.numFound === 0) {
+        errorDiv.innerText = 'Books Not Found'
+        return;
+    }
+    // Create dynamic div for count total search item 
+    const getBooksNumber = document.createElement('div')
+    getBooksNumber.innerHTML = `<h6 class="text-dark text-center">Number of Books Found : ${books.numFound}</h6>`
+    totalBooks.appendChild(getBooksNumber);
+    // Get first 30 books details to display website 
+    books.docs.slice(0, 29).forEach((book) => {
+        // Undefined error handle 
+        const imgId = book.cover_i === undefined ? '8236407' : book.cover_i;
+        const author = book.author_name === undefined ? 'Not Found' : book.author_name;
+        const publishedDate = book.first_publish_year === undefined ? 'Not Found' : book.first_publish_year;
+        const publisher = book.publisher[0] === undefined ? 'Not Found' : book.publisher[0];
+        // Push dynamic HTML 
         const div = document.createElement('div');
-        div.classList.add('col');
         div.innerHTML = `
-                <div onclick="" class="card h-100">
-                <img src="" class="card-img-top" alt="">
-                <div class="card-body">
-                <h5 class="card-title"> Title: ${book.title}</h5>
-                <h6 class="card-title">Author: ${book.author_name}</h6>
-                <p class="card-text">First published: ${book.first_publish_year}</p>
+            <div class = "border border-success dynamic-result card m-1 my-4 shadow-lg p-2"
+            style = "width: 18rem;" >
+                <img src="https://covers.openlibrary.org/b/id/${imgId}-M.jpg" class="card-img-top img-fluid" alt="...">
+                <div class="card-body text-black">
+                    <h5 class="card-title">Book's Name: ${book.title}</h5>
+                    <p class="card-text m-1"><b>Author-Name:</b> ${author}</p>
+                    <p class="card-text m-1"><b>1st Published Year:</b> ${publishedDate}</p>
+                    <p class="card-text m-1"><b>Publisher:</b> ${publisher}</p>
+
                 </div>
-                </div>
+            </div>
         `;
-        searchResult.appendChild(div);
-
-
+        parentDiv.appendChild(div)
     })
-
 }
